f5549614509fae53a4ac23e70bd97af4fe48f4ab
shashwat172002 <shashwatmani17@gmail.com>
2024-03-04 20:00:57 +0530

    Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -17,10 +18,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
-        
+        <Route path="/projects" element={<Projects />} />  
       </Routes>
-        
+      <Footer />    
     </BrowserRouter>
   )
 }
